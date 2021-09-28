f7d51aa2145801d76a5149906f9a168c84df6056
aarce <Arce.Alexis@hotmail.com>
2021-09-28 18:17:13 -0300

    Adds jquery event handler

--- src/js/Producto.js
+++ src/js/Producto.js
@@ -1,8 +1,33 @@
+import numeral from "numeral";
+
+numeral.register("locale", "ar", {
+  delimiters: {
+    thousands: ".",
+    decimal: ",",
+  },
+});
+
+numeral.locale("ar");
+
 class Producto {
-  constructor(nombre, precio, cantidad) {
+  constructor(id, nombre, precio) {
+    this.id = parseInt(id);
     this.nombre = nombre;
-    this.precio = precio;
-    this.cantidad = cantidad;
+    this.precio = parseFloat(precio);
+  }
+
+  renderCard() {
+    let image = `https://picsum.photos/${200 + this.id}`;
+    let price = numeral(this.precio).format("0.00");
+
+    return `<div class="card" style="width: 15rem;">                  
+              <img src="${image}" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">${this.nombre}</h5>
+                <p class="card-text">$ ${price}</p>                    
+                <a href="#" id='${this.id}' class="btn btn-primary btn-agregar">Agregar</a>
+              </div>
+            </div>`;
   }
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,32 @@
-import Producto from "./producto";
+import Producto from "./Producto";
 import $ from "jquery";
 
 const carrito = [];
+const productos = [];
+
+productos.push(new Producto(1, "DONA", 6500));
+productos.push(new Producto(2, "PAN", 120.65));
+productos.push(new Producto(3, "MEDIA LUNA", 60));
+productos.push(new Producto(4, "CHURROS", 75));
+productos.push(new Producto(5, "CHIPA", 100));
+
+renderCards(productos, "#cards");
+
+function renderCards(items, id) {
+  items.forEach((item) => {
+    $(id).append(item.renderCard());
+  });
+}
+
+$("#cards").on("click", ".btn-agregar", agregar_Click);
+
+function agregar_Click(e) {
+  e.preventDefault();
+  carrito.push($(this).prop("id"));
+  console.log(carrito);
+}
+
+/*
 let agregarProducto = true;
 
 while (agregarProducto) {
@@ -28,6 +53,17 @@ carrito.forEach((item) => {
 });
 
 console.log(`Total: $ ${total}`);
+*/
 
-const $divs = $("div"); //
-console.log($divs);
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
