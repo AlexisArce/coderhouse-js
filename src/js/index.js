e4b09f79fffa9b6e144abd3a39d1a138615dd299
aarce <Arce.Alexis@hotmail.com>
2021-10-14 13:28:06 -0300

    Adds delete product event

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,19 @@ import { setNumeral } from "./configNumeral";
 import numeral from "numeral";
 import $ from "jquery";
 import "bootstrap";
+import Swal from "sweetalert2";
 
-setNumeral();
-loadProducts();
+$(function () {
+  setNumeral();
+  loadProducts();
+  updateCartButton();
 
-updateCartButton();
-
-$("#cards").on("click", ".btn-agregar", agregar_Click);
-$("#ver-carrito").on("click", verCarrito_Click);
-$(".filter").on("click", filter_Click);
+  $("#cards").on("click", ".btn-agregar", agregar_Click);
+  $("#ver-carrito").on("click", verCarrito_Click);
+  $(".filter").on("click", filter_Click);
+  $(".table").on("click", ".deleteProduct", eliminar_Click);
+  $("#btnFinalizarCompra").on("click", finalizarCompra_Click);
+});
 
 function agregar_Click(e) {
   e.preventDefault();
@@ -25,6 +29,40 @@ function agregar_Click(e) {
 
   localStorage.setItem("cart", JSON.stringify(cart));
   updateCartButton();
+
+  $("#productAddedToast").toast("show");
+}
+
+function verCarrito_Click(e) {
+  e.preventDefault();
+  renderCart();
+}
+
+function filter_Click(e) {
+  e.preventDefault();
+  let filter = $(this).data("filter");
+  loadProducts(filter);
+}
+
+function eliminar_Click(e) {
+  e.preventDefault();
+  const id = $(this).data("id");
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const currentCart = cart.filter((p) => p.id != id);
+
+  localStorage.setItem("cart", JSON.stringify(currentCart));
+  renderCart();
+  updateCartButton();
+  $(".tooltip").hide();
+}
+
+function finalizarCompra_Click(e) {
+  e.preventDefault();
+  localStorage.setItem("cart", JSON.stringify([]));
+  renderCart();
+  updateCartButton();
+  $("#carrito-modal").modal("hide");
+  Swal.fire("Compra exitosa", "Muchas gracias! vuelva prontoss", "success");
 }
 
 function updateCartButton() {
@@ -37,69 +75,58 @@ function updateCartButton() {
     );
 }
 
-function verCarrito_Click(e) {
-  e.preventDefault();
-
+function renderCart() {
   const cart = JSON.parse(localStorage.getItem("cart")) || [];
-  const products = JSON.parse(localStorage.getItem("products"));
+  const $btnFinalizarCompra = $("#btnFinalizarCompra");
+  const $itemsTable = $("#items-table");
+  const $carritoModal = $("#carrito-modal");
+
+  if (cart && cart.length === 0) {
+    $itemsTable.find("tbody").empty();
+    $itemsTable
+      .find("tfoot")
+      .empty()
+      .append(
+        `<td></td><td><strong>Carrito vacío :(</strong></td><td></td><td></td>`
+      );
+
+    $btnFinalizarCompra.hide();
+    $carritoModal.modal("show");
+    return;
+  }
+
   let total = 0;
   let items = cart.map((i) => {
     total += i.price;
     let formattedPrice = numeral(i.price).format("0.00");
     return `<tr> <th scope="row">
-      <img
-        class="img-fluid"
-        width="30"
-        height="30"
-        src="${i.image}"
-      />
-    </th>
-    </td><td>${i.name}</td>
-    <td style="text-align:right">${formattedPrice}<td></tr>`;
+        <img
+          class="img-fluid"
+          width="30"
+          height="30"
+          src="${i.image}"
+        />
+      </th>
+      </td><td>${i.name}</td>
+      <td>${formattedPrice}<td>
+      <td><a href="#" data-id=${i.id} style="color:white;" class="deleteProduct" data-toggle="tooltip" data-placement="right" title="Eliminar producto">
+         <i class="bi bi-trash"></i>
+          </a>
+      </td>
+    </tr>`;
   });
 
-  const $itemsTable = $("#items-table");
-  const $btnFinalizarCompra = $("#btnFinalizarCompra");
+  let formattedTotal = numeral(total).format("0.00");
+  $itemsTable.find("tbody").empty().append(items);
 
-  if (!items || items.length === 0) {
-    $itemsTable
-      .find("tfoot")
-      .empty()
-      .append(`<td></td><td><strong>Carrito vacío :(</strong></td><td></td>`);
-    $btnFinalizarCompra.hide();
-  } else {
-    let formattedTotal = numeral(total).format("0.00");
-    $itemsTable.find("tbody").empty().append(items);
+  $itemsTable
+    .find("tfoot")
+    .empty()
+    .append(
+      `<td></td><td></td><td><strong>TOTAL</strong></td><td style="text-align:right"><strong>${formattedTotal} $</strong></td>`
+    );
+  $btnFinalizarCompra.show();
 
-    $itemsTable
-      .find("tfoot")
-      .empty()
-      .append(
-        `<td></td><td><strong>TOTAL</strong></td><td style="text-align:right"><strong>${formattedTotal} $</strong></td>`
-      );
-    $btnFinalizarCompra.show();
-  }
-
-  $("#carrito-modal").modal("show");
-}
-
-function filter_Click(e) {
-  e.preventDefault();
-
-  const styles = e.currentTarget.classList;
-  const female = "Female";
-  const male = "Male";
-  const genderless = "Genderless";
-
-  let filter = "";
-
-  if (styles.contains(female)) {
-    filter = female;
-  } else if (styles.contains(male)) {
-    filter = male;
-  } else if (styles.contains(genderless)) {
-    filter = genderless;
-  }
-
-  loadProducts(filter);
+  $('[data-toggle="tooltip"]').tooltip();
+  $carritoModal.modal("show");
 }
